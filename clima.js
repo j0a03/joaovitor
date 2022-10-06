6634b75500b67b783a53ea7a12ef8f155bbf1ba6
j0a03 <johnsupergeeks@gmail.com>
2022-10-06 19:27:04 -0300

    teminando de estilizar o site

--- clima.js
+++ clima.js
@@ -5,8 +5,48 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-// git commandos
-// git add .
+
+const API_KEY = '3735ba9e845abce483f61b3ad24b44ba'
+// datas e temporizador
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+  const minutes = time.getMinutes();
+  const ampm = hour >=12 ? 'PM' : 'AM'
+
+  timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+
+        hoursIn12HrFormat : hoursIn12HrFormat) + ':' +
+        (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">
+        ${ampm}</span>`
+  dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+getWeatherData();
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition((success)=>{
+        console.log(success);
+        let {latitude, longitude} = success.coords
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+         .then(resposta => resposta.json())
+         .then(data =>{
+           console.log(data);
+        })
+    })
+}
+
+
+
+
+
+// git commandos 
+// git add . 
 // git commit -m "terminando de estilizar o site"
-// git push <github.com-
+// git push <github.com-NickDoServSuperGeeks> -> link do site
